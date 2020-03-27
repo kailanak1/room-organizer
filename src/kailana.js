775555ac138894a5cd0f6c7f67a9d695e75cf3c8
kailana <kailanakahawaii@gmail.com>
2020-03-26 17:16:34 -0700

    added some form starters

--- src/kailana.js
+++ src/kailana.js
@@ -37,3 +37,37 @@ function deleteItemP(p){
     parentCard = div.querySelector("card-body")
     parentCard.removeChild(p);
 }
+
+
+///item edit form 
+
+  const editItemForm = document.createElement('form')
+  const inputEditItemName = document.createElement("input");
+  inputEditItemName.placeholder = `Change item's name...`;
+  inputEditItemName.type = "text";
+  inputEditItemName.name = "targetItemName";
+
+  const inputEditItemCategory = document.createElement
+
+  const editItemBtn = document.createElement("input");
+  inputBtn.type = "submit";
+
+
+
+///HTML edit item form 
+
+/* <form action = "http://localhost:3000/api/v1/items" method = PUT>
+<h1>Item Category</h1>
+<fieldset>
+<select id = "itemCategory">
+ <option value = "targetItemCategory">Clothes</option>
+ <option value = "targetItemCategory">Books</option>
+ <option value = "targetItemCategory">Papers</option>
+ <option value = "targetItemCategory">Komono (Misc)</option>
+ <option value = "targetItemCategory">Memento</option>
+</select>
+<input type = "button"
+  value = "change color"
+  onclick = "changeColor()" />
+</fieldset>
+</form> */
